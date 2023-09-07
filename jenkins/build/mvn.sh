e56b70ca72f684f1aa224898dcd780f652e7794e
chaitanya <chaitanya@gamil.com>
2023-09-07 12:25:27 -0400

    file moduf8es

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -5,4 +5,4 @@ echo "**************************"
 echo "** building jar **"
 echo "***************************"
 
-docker run --rm -v /home/jenkins/jenkins_data/jenkins-docker/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:3-alpine "$@"
+docker run --rm -v /home/jenkins/jenkins_data/jenkins_home/workspace/Maven-app/java-app:/app -v /root/.m2:/root/.m2 -w /app maven:3-alpine "$@"
