51fce49b0ba49b7a2d4130ce20040974a02fedd1
chaitanya <chaitanya@gamil.com>
2023-09-07 12:14:51 -0400

    file modiffied

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-scp -i /home/jenkins/cred.pem jenkins/deploy/publish.sh ubuntu@54.197.24.30:/tmp/publish.sh
+scp -i /opt/cred.pem jenkins/deploy/publish.sh ubuntu@54.197.24.30:/tmp/publish.sh
 
-ssh -i /home/jenkins/cred.pem ubuntu@54.197.24.30 "echo maven-app >/tmp/auth.txt && echo Chaitanya@123 >>/tmp/auth.txt && echo 1 >>/tmp/auth.txt &&chmod +x /tmp/publish.sh && /tmp/publish.sh"
+ssh -i /opt/cred.pem ubuntu@54.197.24.30 "echo maven-app >/tmp/auth.txt && chmod +x /tmp/publish.sh && /tmp/publish.sh"
