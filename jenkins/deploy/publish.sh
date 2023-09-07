35c704279a9638b6bb8e9b20d6668065c7e6a602
Chaitanya305 <113754370+Chaitanya305@users.noreply.github.com>
2023-09-07 22:38:09 +0530

    Update publish.sh

--- jenkins/deploy/publish.sh
+++ jenkins/deploy/publish.sh
@@ -4,7 +4,7 @@ export IMAGE=$(sed -n "1p" /tmp/auth.txt)
 export PASS=$(sed -n "2p" /tmp/auth.txt)
 export BUILD_TAG=$(sed -n "3p" /tmp/auth.txt)
 
-docker login -u chaitanya305 -p $PASS
+docker login -u chaitanya305 -P $PASS
 
 cd ~/maven 
 
