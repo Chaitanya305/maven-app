a46ee19d474e27092a4a067b811d9abb1cee7a9b
chaitanya <chaitanya@gamil.com>
2023-09-07 12:57:35 -0400

    file moduf8es

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -2,4 +2,4 @@
 
 scp -i /opt/cred.pem jenkins/deploy/publish.sh ubuntu@54.197.24.30:/tmp/publish.sh
 
-ssh -i /opt/cred.pem ubuntu@54.197.24.30 "echo maven-app >/tmp/auth.txt && chmod +x /tmp/publish.sh && /tmp/publish.sh"
+ssh -i /opt/cred.pem ubuntu@54.197.24.30 "echo maven-app >/tmp/auth.txt && echo $PASS >/tmp/auth.txt && echo $BUILD_TAG >/tmp/auth.txt && chmod +x /tmp/publish.sh && /tmp/publish.sh"
--- jenkins/deploy/publish.sh
+++ jenkins/deploy/publish.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 export IMAGE=$(sed -n "1p" /tmp/auth.txt)
-#export PASS=$(sed -n "2p" /tmp/auth.txt)
-#export BUILD_TAG=$(sed -n "3p" /tmp/auth.txt)
+export PASS=$(sed -n "2p" /tmp/auth.txt)
+export BUILD_TAG=$(sed -n "3p" /tmp/auth.txt)
 
 docker login -u chaitanya305 -p $PASS
 
